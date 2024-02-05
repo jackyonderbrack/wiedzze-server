f0569ed0dc9d1ca42ac3d1fe40594243c5bf582a
Michał Łuczak <mike.projektowanie@gmail.com>
2024-02-05 22:18:44 +0100

    feat: get current user name

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,6 +2,27 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/user.model';
 
-const secretKey = process.env.JWT_SECRET || 'noJwtSecret';
+const secretKey = process.env.JWT_SECRET || '';
 
-export const getCurrentUser = async (req: Request, res: Response, next: NextFunction) => {};
+export const getCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'Brak tokenu autoryzacyjnego' });
+        }
+        const decoded = jwt.verify(token, secretKey) as { id: string };
+
+        const user = await userModel.findById(decoded.id).select('-userPassword');
+        if (!user) {
+            return res.status(404).json({ message: 'Nie znaleziono użytkownika' });
+        }
+        res.json(user);
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(403).json({ message: 'Nieprawidłowy token.' });
+        } else {
+            console.error('Błąd podczas weryfikacji tokenu:', error);
+            return res.status(500).json({ message: 'Wewnętrzny błąd serwera.' });
+        }
+    }
+};
